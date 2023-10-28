bcfb4ce1e25b7560eb9281391db56efa604f776c
starszennn <112663944+starszennn@users.noreply.github.com>
2023-10-29 00:18:05 +0300

    Add files via upload

---
+++ src/components/ScoreView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ScoreView.css';
+
+const ScoreView = ({handleResetClick, score}) => {
+  return (
+    <div>
+      <p>You scored {score} out of 5</p>
+      <button onClick={handleResetClick}>Reset</button>
+    </div>
+  )
+}
+
+export default ScoreView;
+
